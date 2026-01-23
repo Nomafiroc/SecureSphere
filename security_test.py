fb04571e94a3e05b387dcc4a856113a6d3e020a8
Nomafiroc <abdulsamiqasime@gmail.com>
2026-01-23 11:14:52 +0500

    testing vulnerable code pushing

--- security_test.py
+++ security_test.py
@@ -1,8 +1,8 @@
 # security_test.py
 def vulnerable_function():
-    user_input = input("Enter something to evaluate: ")
+    input = input("Enter something to evaluate: ")
     # HIGH SEVERITY: eval is dangerous as it executes raw strings as code
-    return eval(user_input) 
+    return eval(input) 
 
 if __name__ == "__main__":
     vulnerable_function()
